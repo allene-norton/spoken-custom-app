5d1313b423d83a1813be5b21b320717aad94a558
Allene Norton <norton.allene@gmail.com>
2025-09-05 19:47:25 -0400

    remove todo

--- src/actions/omny.ts
+++ src/actions/omny.ts
@@ -47,30 +47,3 @@ export async function getClipsByPlaylist(playlistId?: string | undefined) {
   const recentClips = clips.Items.slice(0, 10).map((item: {Clip: Clip}) => item.Clip);
   return recentClips
 }
-
-/* 
-TODO: Sept. 3, 2025:
-Get all playlists for a network (done)
-  > Create type for playlist (done)
-  > Sort by last modified date (done, automatic)
-Get clips by playlist (done)
-  > Create type for clips (done)
-   commit before import v0
-
-Render 10 latest clips for network on home page (done)
-  > Status icon, image, name, publish date (done)
-  > Color by status (done)
-
-Render program images as tiles (done)
-
-Create array of clip objects for 3 most recent playlists
-  > Sort by ModifiedAtUtc latest - earliest
-
-Clip >
-  Render clip image, title, description, status, publish date
-
-Program > 
-  Render playlists on the left
-  Render latest clips for latest playlists on the right
-
-*/
